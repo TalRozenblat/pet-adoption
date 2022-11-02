942f4227fddb76693c7184c4f398fe47fc1e529c
Tal Rozenblat <rozenblattal@gmail>
2022-11-02 09:30:10 +0200

    added basic dependencies and settings to server.js

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,20 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+import "dotenv/config";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
+
 const PORT = 3000;
+
+
+app.use (express.json());
+app.use(cookieParser());
+app.use(cors({origin: 'http://localhost:3000'}));
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req,res) => {
     req.setEncoding('Hello World!');
 });
 
-app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${PORT}`);
-});
+app.listen(process.env.PORT, () => console.log(`Listening at http://localhost:${process.env.PORT}`));
